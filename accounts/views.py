2bb3d57e519f024a277e9552686e17f9e915b5c4
KAMOLIDDIN <chust1222@gmail.com>
2021-07-25 11:17:29 +0500

    some changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login_required_decorator(func):
+    return login_required(func, login_url='login')
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
